dac79a861b23041dd8030219076dfbf9ff85de76
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-16 11:29:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,12 @@ end
 
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  first_letter = text.scan(/\w+/)[0]
+  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+    true
+  else  
+    false
+  end
 end
 
 def valid_phone_number?(phone)
