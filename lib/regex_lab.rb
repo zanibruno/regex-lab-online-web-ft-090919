e8ea21f81dafb2dbb46897cc06be37b6453e5da7
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-16 11:30:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,3 +26,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 
+def valid_phone_number?(phone)
+  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true
+  else
+    false
+  end
+end
