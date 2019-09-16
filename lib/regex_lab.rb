4faa8cdd61d2323234403ad09aea0f8d91faa0cc
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-16 11:23:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ end
 
 def words_five_letters_long(text)
 result_array = text.split(" ")
-result_array.grep
+result_array.grep(/$)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
